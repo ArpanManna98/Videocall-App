9a345f71d445463a9d950463dab2d450a71d762f
ArpanManna98 <arpanmanna98@gmail.com>
2023-04-03 21:35:02 +0530

    Complete

---
+++ lib/contants.dart
@@ -0,0 +1,6 @@
+class MyConst {
+  // add your appId and appSign from console
+  static const int appId = 1741869305;
+  static const String appSign =
+      "cf73c486ad6e177e7eadf8060b8a589f00ef71d14fd3dc9b95df7cea9659b4c6";
+}
